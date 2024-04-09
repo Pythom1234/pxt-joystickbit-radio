2921bd70b8e98f1446a7039428c239ac9f136c9e
Pythom1234 <pythom1234@email.cz>
2024-04-09 14:19:41 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
-basic.forever(function () {
-	
-})
+//% block="Joystick:bit" color=#184681
+namespace joystickbit {
+    //% block="run joystickbit service radio address $addres"
+    export function runService() {}
+}
