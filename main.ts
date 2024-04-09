f32a11461c931bba96e4cd1120067a631614cdd5
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-04-09 14:28:46 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 //% block="Joystick:bit" color=#184681
 namespace joystickbit {
-    //% block="run joystickbit service on radio address $address" block.loc.cs="spustit službu joystickbit na radio adrese $address"
+    //% block="run joystickbit service on radio address $address"
+    //% block.loc.cs="spustit službu joystickbit na rádio adrese $address"
+    //% group="transmitter"
+    //% group.loc.cs="vysílač"
     export function runService(address: number) {}
 }
